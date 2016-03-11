fd82f5671f62a56e1f8c8f9a2141b89ced9aec64
Matt Miller <thisismattmiller@gmail.com>
2016-03-11 15:13:01 -0500

    ingest items

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ function IngestMms () {
    * @param  {function} cb - Nothing returned
    */
   this.ingestCollections = require(`${__dirname}/lib/collections`)
+
+  /**
+   * Ingest the MMS collections
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.ingestContainers = require(`${__dirname}/lib/containers`)
+
+  /**
+   * Ingest the MMS Items
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.ingestItems = require(`${__dirname}/lib/items`)
 }
 
 module.exports = exports = new IngestMms()
