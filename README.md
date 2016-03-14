40c38f64b0bb639b11b9963ce2902af797a7510d
Matt Miller <thisismattmiller@gmail.com>
2016-03-14 11:57:04 -0400

    mms prune methods

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ingest-mms
+
+[![travis](https://travis-ci.org/nypl-registry/ingest-mms.svg)](https://travis-ci.org/nypl-registry/ingest-mms/)
+
 MMS to native JSON ingest
--- index.js
+++ index.js
@@ -28,6 +28,46 @@ function IngestMms () {
    * @param  {function} cb - Nothing returned
    */
   this.ingestCaptures = require(`${__dirname}/lib/captures`)
+
+  /**
+   * Prune - pruneIgnoreCollections goes through the list of collection uuids and removes the blacklisted ones
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.pruneIgnoreCollections = require(`${__dirname}/lib/prune`).pruneIgnoreCollections
+
+  /**
+   * Prune - pruneItemsMarkedAsExternal and remove external collections
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.pruneItemsMarkedAsExternal = require(`${__dirname}/lib/prune`).pruneItemsMarkedAsExternal
+
+  /**
+   * Prune - pruneItemsWithInvalidContainer removes items that have a container uuid but it is not valid
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.pruneItemsWithInvalidContainer = require(`${__dirname}/lib/prune`).pruneItemsWithInvalidContainer
+
+  /**
+   * Prune - pruneContainersWithoutCaptures removes items and containers that do not have any captures
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.pruneContainersWithoutCaptures = require(`${__dirname}/lib/prune`).pruneContainersWithoutCaptures
+  /**
+   * Prune - pruneCollectionsWithoutCaptures removes collections (items + containers) that do not have any captures
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.pruneCollectionsWithoutCaptures = require(`${__dirname}/lib/prune`).pruneCollectionsWithoutCaptures
+  /**
+   * Prune - pruneCollectionsWithoutCaptures removes items that do not have any captures
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.pruneItemsWithoutCaptures = require(`${__dirname}/lib/prune`).pruneItemsWithoutCaptures
 }
 
 module.exports = exports = new IngestMms()
